da7011e9d92f41983df461e59f389714747b90ac
Pure Krome <pure.krome@world-domination.com.au>
2018-07-20 17:48:51 +1000

    Added a Simple Stock app.

---
+++ src/SimpleStockApp/Stock.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleStockApp
+{
+    public class Stock
+    {
+        public string Symbol { get; set; }
+        public string CompanyName { get; set; }
+        public string PrimaryExchange { get; set; }
+        public string Price { get; set; }
+    }
+}
