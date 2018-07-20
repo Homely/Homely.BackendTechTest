106ed4cdbaa4d711ac3cec6a5c1b27a057fd211e
Ryan Miranda <rpmir1@gmail.com>
2018-07-20 16:18:24 +1000

    Added "InefficientCode" project.

---
+++ src/InefficientCode/Disk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace InefficientCode
+{
+    public class Disk
+    {
+        public Dictionary<string, string> ReadAllFilesInDirectory(string directory)
+        {
+            Dictionary<string, string> fileNameAndContents = new Dictionary<string, string>();
+
+            foreach (string filename in Directory.EnumerateFiles(directory))
+            {
+                string contents = File.ReadAllText(filename);
+
+                fileNameAndContents.Add(filename, contents);
+            }
+
+            return fileNameAndContents;
+        }
+    }
+}
---
+++ src/InefficientCode/Networking.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace InefficientCode
+{
+    public class Networking
+    {
+        public async Task<IEnumerable<HttpResponseMessage>> GetResourcesAsync()
+        {
+            HttpResponseMessage resourceOne = await GetResource1Async();
+            HttpResponseMessage resourceTwo = await GetResource2Async();
+
+            var resources = new[]
+            {
+                resourceOne,
+                resourceTwo
+            };
+
+            return resources;
+        }
+
+        private async Task<HttpResponseMessage> GetResource1Async()
+        {
+            using (var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://www.somewebsite.com")
+            })
+            {
+                return await httpClient.GetAsync("one");
+            }
+        }
+
+        private async Task<HttpResponseMessage> GetResource2Async()
+        {
+            using (var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://www.somewebsite.com")
+            })
+            {
+                return await httpClient.GetAsync("two");
+            }
+        }
+    }
+}
